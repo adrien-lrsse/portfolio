dd30a681dda73e07e7c2cf75c07e835ce063c5df
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2025-01-03 16:30:40 -0500

    feat: add Logos component with sliding animation and styling

---
+++ src/components/Logos/Logos.tsx
@@ -0,0 +1,60 @@
+import './logos.css'
+import htmlLogo from '../../assets/logos/html.png'
+import jsLogo from '../../assets/logos/js.png'
+import reactLogo from '../../assets/logos/react.png'
+import dockerLogo from '../../assets/logos/docker.png'
+import nodeLogo from '../../assets/logos/node.png'
+import cLogo from '../../assets/logos/c.png'
+import expressLogo from '../../assets/logos/express.png'
+import flutterLogo from '../../assets/logos/flutter.png'
+import goLogo from '../../assets/logos/go.png'
+import javaLogo from '../../assets/logos/java.png'
+import npmLogo from '../../assets/logos/npm.png'
+import pythonLogo from '../../assets/logos/python.png'
+import sqliteLogo from '../../assets/logos/sqlite.png'
+import svelteLogo from '../../assets/logos/svelte.png'
+import viteLogo from '../../assets/logos/vite.png'
+
+
+export default function Logos() {
+    return (
+        <div className="logos">
+            <div className="logos-slide">
+            <img src={htmlLogo} alt="HTML Logo" title="HTML" />
+            <img src={jsLogo} alt="JavaScript Logo" title="JavaScript" />
+            <img src={reactLogo} alt="React Logo" title="React" />
+            <img src={dockerLogo} alt="Docker Logo" title="Docker" />
+            <img src={nodeLogo} alt="Node.js Logo" title="Node.js" />
+            <img src={cLogo} alt="C Logo" title="C" />
+            <img src={expressLogo} alt="Express Logo" title="Express" />
+            <img src={flutterLogo} alt="Flutter Logo" title="Flutter" />
+            <img src={goLogo} alt="Go Logo" title="Go" />
+            <img src={javaLogo} alt="Java Logo" title="Java" />
+            <img src={npmLogo} alt="NPM Logo" title="NPM" />
+            <img src={pythonLogo} alt="Python Logo" title="Python" />
+            <img src={sqliteLogo} alt="SQLite Logo" title="SQLite" />
+            <img src={svelteLogo} alt="Svelte Logo" title="Svelte" />
+            <img src={viteLogo} alt="Vite Logo" title="Vite" />
+            </div>
+
+            <div className="logos-slide">
+
+            <img src={htmlLogo} alt="HTML Logo" title="HTML" />
+            <img src={jsLogo} alt="JavaScript Logo" title="JavaScript" />
+            <img src={reactLogo} alt="React Logo" title="React" />
+            <img src={dockerLogo} alt="Docker Logo" title="Docker" />
+            <img src={nodeLogo} alt="Node.js Logo" title="Node.js" />
+            <img src={cLogo} alt="C Logo" title="C" />
+            <img src={expressLogo} alt="Express Logo" title="Express" />
+            <img src={flutterLogo} alt="Flutter Logo" title="Flutter" />
+            <img src={goLogo} alt="Go Logo" title="Go" />
+            <img src={javaLogo} alt="Java Logo" title="Java" />
+            <img src={npmLogo} alt="NPM Logo" title="NPM" />
+            <img src={pythonLogo} alt="Python Logo" title="Python" />
+            <img src={sqliteLogo} alt="SQLite Logo" title="SQLite" />
+            <img src={svelteLogo} alt="Svelte Logo" title="Svelte" />
+            <img src={viteLogo} alt="Vite Logo" title="Vite" />
+            </div>
+        </div>
+    );
+}
